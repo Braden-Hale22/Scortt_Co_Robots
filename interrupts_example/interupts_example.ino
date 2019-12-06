e5d5e16ae8d1eca2aa01bc806226cf2f817d5094
Braden-Hale22 <58077460+Braden-Hale22@users.noreply.github.com>
2019-12-06 08:30:21 -0500

    Update interupts_example.ino

--- interrupts_example/interupts_example.ino
+++ interrupts_example/interupts_example.ino
@@ -11,30 +11,30 @@ void setup()
   pinMode(6,OUTPUT); //the pin for the LED
   pinMode(11,INPUT); //the pin for the main power button
   pinMode(2,INPUT); //the pin for the aux button it has to be wired into pin to because thats how the chip is made.
-  attachInterrupt(digitalPinToInterrupt(2),blink , RISING);
+  attachInterrupt(digitalPinToInterrupt(2),blink , RISING); // this line attaches the interrupt pin to the function (Blink) that we want it to run
 }
 
 
 void loop()
 {
-  if(mainButton==true)
+  if(mainButton==true)            //Here we check the status of the main on/off button andd if its true
   {
-    auxButton=digitalRead(11);
-    if(auxButton==false)
+    auxButton=digitalRead(11);    //if mainButton is true then we read the state of the auxButton
+    if(auxButton==false)          //we had to change the way the buttons work to pull up resistors instead of pull down.
     {
-      digitalWrite(6,HIGH);
+      digitalWrite(6,HIGH);       //if the button is pressed (true) then we turn on the led
     }
     else
     {
-      digitalWrite(6,LOW);
+      digitalWrite(6,LOW);        //if the button is not pressed write the led Low
     }
   }
 }
 
 
-void blink()
+void blink()                      // this is the function that runs when the interupt is called
 {
-  mainButton=!mainButton; 
-}
+  mainButton=!mainButton;         // this startement takes the state of the variable mainButton and inverts it
+}                                 //if its true it turns false, of its false it turns true
 
  
